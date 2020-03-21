11876bbd62c1715036dbce8a5b4c23767ef25785
jenseneducation-siba-kouki <54539086+jenseneducation-siba-kouki@users.noreply.github.com>
2020-03-21 18:30:31 +0100

    about page but not finished

--- airbean/src/views/About.vue
+++ airbean/src/views/About.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Vårt Kaffe</h1>
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+       Obcaecati, ex! Quo, enim harum! Vel aperiam quidem ipsa porro <br>
+        eaque molestiae corporis voluptatem temporibus hic voluptatum, <br>
+         recusandae repellendus amet laborum cupiditate! <br> </h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+         Aperiam voluptatibus, repellat numquam pariatur delectus dolore
+         velit libero ullam odio eos minima. Quis ipsam exercitationem 
+         doloremque nisi, esse ipsa! Praesentium exercitationem perferendis,
+         debitis ut dolores iusto possimus. Illo, enim incidunt. <br>
+         Natus repudiandae dolorem autem harum consequuntur voluptatum in,
+         provident ipsa ab quae tenetur ullam totam placeat laborum eum maiores 
+         optio excepturi architecto quo nihil incidunt. Id, reprehenderit obcaecati
+         provident aperiam excepturi ex at sed et, earum saepe labore voluptas 
+         asperiores voluptatum ut quos expedita quo! Inventore in repellat harum
+         quibusdam quaerat illo obcaecati tempora unde voluptatum iste exercitat
+         ionem dignissimos, consequatur aspernatur corrupti repellendus dolores
+         veritatis mollitia vel doloribus blanditiis facilis doloremque recusandae</p>   
+     <img src="@/assets/graphics/eva-cortado.jpg" alt="eva">
+     <h2>Eva Cortado</h2>
+     <h4>VD & Grundare</h4>
   </div>
 </template>
 <script>
