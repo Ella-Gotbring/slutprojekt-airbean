cf4e0dc38416d143a29642aacb6eec6906b01731
jenseneducation-siba-kouki <54539086+jenseneducation-siba-kouki@users.noreply.github.com>
2020-03-22 19:40:21 +0100

    add list

---
+++ airbean/src/components/MenuList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="menulist">
+    <router-link to="/menu">Meny</router-link>
+    <router-link to="/about">Vårt Kaffe</router-link>
+    <router-link to="/status">OrderStatus</router-link>
+
+  </div>
+</template>
+
+<script>
+import Navicon from "./../components/Navicon"
+export default {
+name: 'MenuList',
+components : {
+    Navicon
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ airbean/src/components/Navicon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Navicon">
+  <img src="@/assets/graphics/navicon.svg" alt="icon">
+  <img src="@/assets/graphics/close.svg" alt="close">
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Navicon'
+}
+</script>
+
+<style>
+
+</style>
