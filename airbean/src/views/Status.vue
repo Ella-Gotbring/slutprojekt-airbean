746630d2bff3b801386d05c6148e986c4b5638a3
jenseneducation-siba-kouki <54539086+jenseneducation-siba-kouki@users.noreply.github.com>
2020-03-22 20:12:14 +0100

    working on order

--- airbean/src/views/Status.vue
+++ airbean/src/views/Status.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="status">
-      
+      <p>Ditt Ordernummer är #12DV231F43SH4</p>
+      <img src="@/assets/graphics/drone.svg" alt="drone">
+      <h1>Din beställning är på väg! </h1>
+      <p>13 minuter</p>
+      <a href="#" @click="router.push('/menu')">OK, Cool!</a>
   </div>
 </template>
 
