6a06bdeb6f14c7b5ba99ac01b5e0fc16cb3a0926
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2020-03-30 21:48:07 +0200

    working on menypage
    working on new menupage, menu not yet showing on page

--- airbean/src/components/Menuitem.vue
+++ airbean/src/components/Menuitem.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="menuitem"></div>
+  <div class="menuitem">
+    <div class="add" @click="add">
+      <button>
+        <img src="../assets/graphics/add.svg" alt="add" />
+      </button>
+    </div>
+    <h2 class="itemtitle">
+      {{item.title}}
+      <span class="dotted-line"></span>
+    </h2>
+    <p class="itemdesc">{{item.desc}}</p>
+    <h2 class="itemprice">{{item.price}}</h2>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Menuitem",
-  methods: {}
+  props: {
+    item: Object
+  },
+  methods: {
+    // add() {
+    //   this.$store.dispatch("additemTocart", this.item);
+    // }
+  }
 };
 </script>
 
