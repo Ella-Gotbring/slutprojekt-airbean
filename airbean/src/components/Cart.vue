ec412f5922c77439aaa0b6f75d296bec8d0bcdba
jenseneducation-siba-kouki <54539086+jenseneducation-siba-kouki@users.noreply.github.com>
2020-03-31 13:07:48 +0200

    working with cart

---
+++ airbean/src/components/Cart.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cart">
+      <h1>Din beställning</h1>
+      <div v-for='item in cart' :key='item.id'>
+        <CartItem :item='item' />
+        </div>
+      <h2>Total</h2>
+      <p>..........................................</p>
+      <h2>{{counter}} kr</h2>
+      <p>inkl moms + drönarleverans</p>
+      <button @click="sendOrder">Take My Money</button>
+  </div>
+</template>
+
+<script>
+import CartItem from './../components/CartItem'
+export default {
+    name: 'Cart',
+    components: {
+      CartItem
+    },
+    computed: {
+      cart(){
+        return this.$store.state.cart;
+      },
+      counter(){
+        return this.$store.state.counter;
+      }
+    },
+    methods:{
+      sendOrder(){
+        this.$router.push('/status')
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
