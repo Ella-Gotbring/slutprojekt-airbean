f16f5002e0186ba243553d92bbd926539d2e75f2
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2020-03-31 23:48:29 +0200

    working on cartitem
    need to fix styling

--- airbean/src/components/CartItem.vue
+++ airbean/src/components/CartItem.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="cart-item">
     <div class="item">
-      <h2>{{item.title}}</h2>
-      <h4>{{item-price}} kr</h4>
+      <h2>
+        {{item.title}}
+        <span class="dots"></span>
+      </h2>
+      <!-- <h4>{{item-price}} </h4> -->
     </div>
-    <p>...........................................</p>
+
+    <!-- <p>...........................................</p> -->
+    <h5 class="total">{{totalamount}}</h5>
     <img src="./../assets/graphics/arrow-up.svg" alt="arrowup" @click="increase" />
-    <h3>{{counter}}</h3>
+    <!-- <h3>{{item.quantity}}</h3> -->
     <img src="./../assets/graphics/arrow-down.svg" alt="arrowdown" @click="decrease" />
   </div>
 </template>
@@ -14,21 +19,33 @@
 <script>
 export default {
   name: "CartItem",
+  props: {
+    item: Object
+  },
   computed: {
-    counter() {
-      return this.$store.state.counter;
+    // counter() {
+    //   return this.$store.state.counter;
+    totalamount() {
+      return this.item.price * this.item.quantity;
+    }
+  },
+  methods: {
+    increase() {
+      // this.store.state.counter++;
+      this.item.quantity++;
     },
-    methods: {
-      increase() {
-        this.store.state.counter++;
-      },
-      decrease() {
-        this.store.state.counter--;
-      }
+    decrease() {
+      // this.store.state.counter--;
+      this.item.quantity--;
     }
   }
 };
 </script>
 
-<style>
+<style lang="scss">
+.item {
+  display: flex;
+  align-items: flex-end;
+  
+}
 </style>
