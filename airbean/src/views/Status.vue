3c435d72c389c616736aa52fde07a46e9c2ba46b
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2020-04-11 22:06:48 +0200

    fixed some styling
    fixed some styling and fixed the loading svg image what wasn't being displayed

--- airbean/src/views/Status.vue
+++ airbean/src/views/Status.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="status">
-    <img v-if="loading" src="../assets/graphics/loader.png" alt="loading icon" />
-
+  <div class="order">
     <h1 v-if="loading">Brewing</h1>
+    <div class="loading" v-if="loading">
+      <img src="../assets/graphics/loader.png" alt="loading icon" />
+    </div>
 
     <h1 class="orderNumber" v-if="order.orderNr">
       Ordernumber:
@@ -35,5 +36,73 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.order {
+  text-align: center;
+  padding: 1.5rem;
+  color: white;
+  background: orange;
+
+  .drone {
+    img {
+      width: 300px;
+      animation: hover 4s linear infinite;
+      padding-top: 25px;
+    }
+    @keyframes hover {
+      0% {
+        transform: translateY(0) rotateZ(0deg);
+      }
+      25% {
+        transform: translateY(0.25rem) rotateZ(1deg);
+      }
+      50% {
+        transform: translateY(0.5rem) rotateZ(0deg);
+      }
+      75% {
+        transform: translateY(0.25rem) rotateZ(-1deg);
+      }
+      100% {
+        transform: translateY(0) rotateZ(0deg);
+      }
+    }
+    h1 {
+      font-family: Georgia, "Times New Roman", Times, serif;
+      font-size: 30px;
+      color: white;
+    }
+  }
+  .eta {
+    font-family: Georgia, "Times New Roman", Times, serif;
+  }
+  .button {
+    font-family: Georgia, "Times New Roman", Times, serif;
+    display: flex;
+    max-width: 14rem;
+    width: 100%;
+    font-size: 1rem;
+    text-decoration: underline;
+    height: 4rem;
+    margin: auto;
+    align-items: center;
+    justify-content: center;
+    color: black;
+    border-radius: 99rem;
+  }
+}
+.loading {
+  padding-top: 250px;
+  animation: none;
+
+  img {
+    width: 100px;
+    padding-top: 25px;
+  }
+}
+.orderNumber {
+  font-size: 15px;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  color: white;
+  padding-top: 22px;
+}
 </style>
